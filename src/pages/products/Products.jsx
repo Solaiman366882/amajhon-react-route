9429efbfabc0d0243afb510bb1bc9b63debac4fa
Md.Solaiman <md.solaiman366882@gmail.com>
2023-09-21 00:59:18 +0600

    Product Cart Added and proptypes handled

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,13 +1,21 @@
 import { useLoaderData } from "react-router-dom";
+import ProductCart from "./ProductCart";
 
 const Products = () => {
 
-    const products = useLoaderData();
+    const newProducts = useLoaderData();
+    const products = newProducts.products;
+
+    
     console.log(products);
 
     return (
         <div>
-            <div className=" min-h-screen flex items-center content-center text-4xl text-rose-950">  This is Home Page</div>
+            <div className=" min-h-screen p-5 grid grid-cols-3 gap-5"> 
+                {
+                    products?.map(product => <ProductCart key={product.id} product={product}></ProductCart>)
+                }
+            </div>
         </div>
     );
 };
